3746e1f13a24729eeb36334d38412f9a7adb4ae1
Benjamin Francois <Benjamin.Francois@disneyanimation.com>
2022-08-19 17:08:58 -0700

    How about object-oriented programming?

--- find_machine.py
+++ find_machine.py
@@ -1,30 +1,13 @@
 #!/usr/bin/env python3
 
-from lxml import etree
+import xml.dom.minidom
 
-MAMEXML='/usr/local/games/mame/mame.xml'
+MAMEXML='./shortmame.xml'
 
-# retrieve the metadata from the XML file
-def game_meta(name,xmlroot,node,meta):
-    """retrieve game metadata"""
-    try:
-        xpath = "./" + node + "[@name='" + name + "']/{}"
-        return xmlroot.findall(xpath.format(meta))[0].text
-    except IndexError:
-        print("Either the game %s, its year or manufacturer, were not found in this database" % name)
-        return None
+class MachineParse(object):
 
-def game_meta_misc(name,xmlroot,node,meta,tag):
-    """retrieve misc game metadata"""
-    try:
-        xpath = "./" + node + "[@name='" + name + "']//" + meta
-        value = xmlroot.findall(xpath)[0].attrib[tag]
-        return value
-    except (IndexError,KeyError):
-        return ''
-
-tree_mame = etree.parse(MAMEXML)
-tree_root = tree_mame.getroot()
-
-print(game_meta('ffight',tree_root,'machine','description'))
-print(game_meta_misc('ffight',tree_root,'machine','input','players'))
+  def __init__(self,is_clone,source):
+    self.name = name
+    self.description = description
+    self.is_clone = is_clone
+    self.source = source
