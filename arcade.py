21548c4629f2071d47034b21195c17e6dfa6ae95
Benjamin FRANCOIS <kwyxz@kwyxz.org>
2022-08-21 12:45:13 -0700

    Fix search regexp

--- arcade.py
+++ arcade.py
@@ -9,10 +9,13 @@ import msg
 import remote_host
 import xml.etree.ElementTree as ET
 
-def find_machine(game,root):
-    return root.findall(f".//*[@name='{game}']")[0].attrib['sourcefile']
+def find_machine(game,root,debug):
+    """looking for hardware a game runs on"""
+    msg.debug(f"looking for hardware for {game}",debug)
+    return root.findall(f".//machine[@name='{game}']")[0].attrib['sourcefile']
 
 def find_by_machine(machine,root,rompath,debug):
+    """list all games running on a hardware"""
     games = []
     for game in root.findall(f".//*[@sourcefile='{machine}']"):
         try:
@@ -37,11 +40,21 @@ def curate(gamelist,remote,mamerompath,mamexml,debug):
     with open(mamexml) as xmlfile:
         xmlstring = xmlfile.read()
     root = ET.fromstring(xmlstring)
+    machines = []
     romset = []
+    # parse through the list of games and find the hardwares that will be pushed
     for game in gamelist:
         msg.debug(f"looking for game {game}",debug)
-        machine = find_machine(game,root)
+        machine = find_machine(game,root,debug)
+        msg.debug(f"found hardware : {machine}",debug)
+        machines.append(machine)
+    # get rid of duplicates
+    sorted_machines = sorted(set(machines))
+    # create a new loop over all the hardwares to optimize search
+    for machine in sorted_machines:
+        msg.debug(f"listing games by hardware : {machine}",debug)
         machine_games = find_by_machine(machine,root,mamerompath,debug)
         romset = romset + machine_games
+    # there should not be any duplicates yet but just to play it safe
     sorted_set = sorted(set(romset))
     remote_host.pushromset(sorted_set,mamerompath,remote['rom_path'] + '/' + 'arcade/',remote,debug)
