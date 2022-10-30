513dbd4b1f85336e7931931aa2d1c9c96b8229d1
kwyxz <kwyxz@kwyxz.org>
2022-10-29 22:35:26 -0700

    Minor changes with user output

--- remote_host.py
+++ remote_host.py
@@ -14,8 +14,9 @@ def push_romset_ssh(romset,local,dest,ip_addr,port,user,debug): # pylint: disabl
     sshcon = paramiko.SSHClient()
     sshcon.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
-        msg.debug(f"SSH:\tconnect to {user}@{ip_addr}:{port}{dest}",debug)
+        msg.debug(f"SSH:\tconnecting to {user}@{ip_addr}:{port}{dest}",debug)
         sshcon.connect(ip_addr, port=port, username=user)
+        msg.ok("SSH:\tconnected to {user}@{ip_addr}:{port}{dest}")
         sftp=sshcon.open_sftp()
         for rom in romset:
             try:
@@ -41,15 +42,17 @@ def push_romset_ssh(romset,local,dest,ip_addr,port,user,debug): # pylint: disabl
                 sftp.put(local_rom,remote_rom,callback=None,confirm=True)
                 msg.ok(f"PUSHED:\t{remote_rom}")
     except paramiko.ssh_exception.NoValidConnectionsError:
-        msg.die("Unable to connect to the remote SSH host, check the network parameters")
+        msg.die(f"SSH:\tunable to connect to remote host, check the network parameters")
     sshcon.close()
+    msg.ok(f"SSH:\tdisconnected")
 
 def push_romset_ftp(romset,local,dest,ip_addr,port,user,passwd,debug): # pylint: disable=too-many-arguments
     """push romset using FTP"""
     ftp = FTP()
-    msg.debug(f"FTP:\tconnecting to {ip_addr}:{port}",debug)
     try:
+        msg.debug(f"FTP:\tconnecting to {ip_addr}:{port}",debug)
         ftp.connect(host=ip_addr,port=int(port))
+        msg.ok(f"FTP:\tconnected to to {ip_addr}:{port}")
         msg.debug(f"FTP:\tlogin={user},password={passwd}",debug)
         ftp.login(user=user,passwd=passwd)
         for rom in romset:
@@ -77,8 +80,9 @@ def push_romset_ftp(romset,local,dest,ip_addr,port,user,passwd,debug): # pylint:
                     ftp.storbinary("STOR " + remote_rom, local_romfile)
                 msg.ok(f"PUSHED:\t{remote_rom}")
     except OSError:
-        msg.die("Unable to connect to the remote FTP host, check the network parameters")
+        msg.die("FTP:\tunable to connect to remote host, check the network parameters")
     ftp.quit()
+    msg.ok(f"FTP:\tdisconnected")
 
 def pushromset(romset,local,folder,remote,debug):
     """select push protocol based on settings"""
